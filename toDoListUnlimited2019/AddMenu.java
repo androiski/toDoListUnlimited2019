40e6f3b0d4f3d148346235c0832da379ba7d536e
Andrew Murza <androiski@gmail.com>
2019-04-10 21:27:53 -0700

    addmenu and mainmenu added with simple gui thingies

--- toDoListUnlimited2019/AddMenu.java
+++ toDoListUnlimited2019/AddMenu.java
@@ -7,10 +7,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JEditorPane;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class AddMenu extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblDescription;
 
 	/**
 	 * Launch the application.
@@ -33,14 +42,73 @@ public class AddMenu extends JFrame {
 	 */
 	public AddMenu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 693, 397);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("TEST");
-		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		textField = new JTextField();
+		textField.setBounds(153, 27, 250, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Name:");
+		lblNewLabel.setBounds(97, 30, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		lblDescription = new JLabel("Description:");
+		lblDescription.setBounds(71, 61, 72, 14);
+		contentPane.add(lblDescription);
+		
+		JLabel lblDueDate = new JLabel("Due Date:");
+		lblDueDate.setBounds(71, 150, 72, 14);
+		contentPane.add(lblDueDate);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"YEAR", "2019", "2020", "2021", "2022", "2023", "2024"}));
+		comboBox.setBounds(153, 147, 60, 20);
+		contentPane.add(comboBox);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"MONTH", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}));
+		comboBox_1.setBounds(223, 147, 99, 20);
+		contentPane.add(comboBox_1);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"DAY", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		comboBox_2.setBounds(332, 147, 60, 20);
+		contentPane.add(comboBox_2);
+		
+		JLabel lblDateStarted = new JLabel("Date Started:");
+		lblDateStarted.setBounds(57, 238, 86, 14);
+		contentPane.add(lblDateStarted);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"YEAR", "2019", "2020", "2021", "2022", "2023", "2024"}));
+		comboBox_3.setBounds(153, 235, 60, 20);
+		contentPane.add(comboBox_3);
+		
+		JComboBox comboBox_4 = new JComboBox();
+		comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"MONTH", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}));
+		comboBox_4.setBounds(223, 235, 99, 20);
+		contentPane.add(comboBox_4);
+		
+		JComboBox comboBox_5 = new JComboBox();
+		comboBox_5.setModel(new DefaultComboBoxModel(new String[] {"DAY", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		comboBox_5.setBounds(332, 235, 60, 20);
+		contentPane.add(comboBox_5);
+		
+		JButton btnNewButton = new JButton("FINISH\r\n");
+		btnNewButton.setBounds(475, 125, 152, 65);
+		contentPane.add(btnNewButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(153, 58, 250, 79);
+		contentPane.add(scrollPane);
+		
+		JEditorPane editorPane = new JEditorPane();
+		scrollPane.setViewportView(editorPane);
 	}
-
 }
--- toDoListUnlimited2019/MainMenu.java
+++ toDoListUnlimited2019/MainMenu.java
@@ -12,6 +12,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JList;
 
 public class MainMenu extends JFrame {
 
@@ -41,34 +45,35 @@ public class MainMenu extends JFrame {
 		setBounds(100, 100, 800, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		setTitle("To Do List Unlimited 2019\r\n");
 		getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Load");
-		btnNewButton.setFont(new Font("Rockwell", Font.PLAIN, 15));
-		btnNewButton.setBounds(447, 281, 125, 50);
-		getContentPane().add(btnNewButton);
-		
-		JLabel lblNewLabel = new JLabel("To Do List Unlimited 2019");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Rockwell", Font.PLAIN, 48));
-		lblNewLabel.setBounds(72, 53, 627, 104);
-		getContentPane().add(lblNewLabel);
-		
 		JButton btnAdd = new JButton("Add");
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-//				frame.dispose();
+
 				AddMenu menuadd = new AddMenu();
 				menuadd.setVisible(true);
 			}
 		});
+		contentPane.setLayout(null);
 		btnAdd.setFont(new Font("Rockwell", Font.PLAIN, 15));
-		btnAdd.setBounds(176, 281, 125, 50);
+		btnAdd.setBounds(181, 366, 120, 27);
 		getContentPane().add(btnAdd);
+		
+		JButton btnNewButton = new JButton("Load");
+		btnNewButton.setFont(new Font("Rockwell", Font.PLAIN, 15));
+		btnNewButton.setBounds(452, 366, 125, 27);
+		getContentPane().add(btnNewButton);
+		
+		JList list = new JList();
+		list.setBounds(26, 39, 704, 266);
+		contentPane.add(list);
+		
+		JList list_1 = new JList();
+		list_1.setBounds(725, 169, 16, 1);
+		contentPane.add(list_1);
 	}
-
 }
